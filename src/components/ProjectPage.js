99537495020dc3dcb363a153cc3edfae158582c9
Riffyraff <82163631+ArifAli999@users.noreply.github.com>
2022-07-18 17:42:11 +0100

    framer

--- src/components/ProjectPage.js
+++ src/components/ProjectPage.js
@@ -1,14 +1,21 @@
 import React from 'react'
 import ProjectComp from './ProjectComp'
+import { motion } from "framer-motion"
+
 
 function ProjectPage() {
     return (
         <div className='mt-20  w-full  mb-20 relative'>
-            <div className='text-lightgray flex  h-[100px] mt-0 p-0 flex-nowrap items-center text-xl font-popp tracking-wider	mb-10 w-full bg-  uppercase '>
+            <div className='text-lightgray flex mb-20 h-[100px] mt-0 p-0 flex-nowrap items-center text-xl font-popp tracking-wider	 w-full bg-  uppercase '>
                 <div className='w-full overflow-clip'>
-                    <h2 className='text-4xl absolute top-0 md:text-8xl whitespace-pre -ml-14 text-lightblack borderh2'> PROJECTS <span className='diffout'>WORK </span>
-                        PROJECTS <span className='diffout'>WORK </span>
-                        PROJECTS <span className='text-gray-'>WORK</span>PROJECTS</h2>
+                    <motion.h2
+                        initial={{ opacity: 0.7, x: -300 }}
+                        whileInView={{ opacity: 1, x: 0 }}
+                        transition={{ duration: 2.5 }}
+                        className='text-4xl absolute top-0 md:text-8xl whitespace-pre -ml-14 text-lightblack borderh2'> PROJECTS <span className='diffout'>MY </span>
+                        WORK<span className='diffout'> PROJECTS </span>
+                        WORK <span className='diffout'>PROJECTS </span>WORK
+                        <span className='text-gray-'> MY </span>WORK </motion.h2>
                 </div>
 
             </div>
